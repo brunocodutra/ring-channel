310e9258ce6c42e9c1471b57b3f700cdef0c8f8f
Bruno Dutra <brunocodutra@gmail.com>
2019-04-06 21:39:51 +0200

    define ring_channel, RingSender and RingReceiver

---
+++ src/sender.rs
@@ -0,0 +1,28 @@
+use crate::{channel::*, error::*};
+use derivative::Derivative;
+
+/// The sending end of a [`ring_channel`].
+///
+/// [`ring_channel`]: fn.ring_channel.html
+///
+/// # Examples
+///
+/// ```rust
+/// // TODO
+/// ```
+#[derive(Derivative)]
+#[derivative(Debug(bound = ""), Clone(bound = ""))]
+pub struct RingSender<T>(#[derivative(Debug = "ignore")] pub Endpoint<T>);
+
+impl<T> RingSender<T> {
+    /// Sends a message through the channel without blocking.
+    ///
+    /// * If the channel is not disconnected, the message is pushed into the internal ring buffer.
+    ///     * If the internal ring buffer is full, the oldest pending message is overwritten.
+    /// * If the channel is disconnected, [`SendError::Disconnected`] is returned.
+    ///
+    /// [`SendError::Disconnected`]: enum.SendError.html#variant.Disconnected
+    pub fn send(&self, value: T) -> Result<(), SendError<T>> {
+        self.0.send(value)
+    }
+}
