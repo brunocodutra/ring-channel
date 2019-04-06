618004a8e9bf8b3df6403d8594d2f4d683ea3f7a
Bruno Dutra <brunocodutra@gmail.com>
2019-04-06 21:39:51 +0200

    define ring_channel, RingSender and RingReceiver

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,42 @@ mod buffer;
 mod channel;
 mod error;
 mod macros;
+mod receiver;
+mod sender;
 
 pub use error::*;
+pub use receiver::*;
+pub use sender::*;
+
+/// Opens a multi-producer multi-consumer channel backed by a ring buffer.
+///
+/// The associated ring buffer can contain up to `capacity` pending messages.
+///
+/// Sending and receiving messages through this channel _never blocks_, however
+/// pending messages may be overwritten if the internal ring buffer overflows.
+///
+/// # Panics
+///
+/// Panics if the `capacity` is `0`.
+///
+/// # Examples
+///
+/// ```rust
+/// // TODO
+/// ```
+pub fn ring_channel<T>(capacity: usize) -> (RingSender<T>, RingReceiver<T>) {
+    assert!(capacity > 0, "capacity must be greater than zero");
+    let channel::RingChannel(left, right) = channel::RingChannel::new(capacity);
+    (RingSender(left), RingReceiver(right))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn ring_channel_panics_on_zero_capacity() {
+        ring_channel::<()>(0);
+    }
+}
---
+++ src/receiver.rs
@@ -0,0 +1,30 @@
+use crate::{channel::*, error::*};
+use derivative::Derivative;
+
+/// The receiving end of a [`ring_channel`].
+///
+/// [`ring_channel`]: fn.ring_channel.html
+///
+/// # Examples
+///
+/// ```rust
+/// // TODO
+/// ```
+#[derive(Derivative)]
+#[derivative(Debug(bound = ""), Clone(bound = ""))]
+pub struct RingReceiver<T>(#[derivative(Debug = "ignore")] pub Endpoint<T>);
+
+impl<T> RingReceiver<T> {
+    /// Receives a message through the channel without blocking.
+    ///
+    /// * If the internal ring buffer isn't empty, the oldest pending message is returned.
+    /// * If the internal ring buffer is empty, [`RecvError::Empty`] is returned.
+    /// * If the channel is disconnected and the internal ring buffer is empty,
+    /// [`RecvError::Disconnected`] is returned.
+    ///
+    /// [`RecvError::Empty`]: enum.RecvError.html#variant.Empty
+    /// [`RecvError::Disconnected`]: enum.RecvError.html#variant.Disconnected
+    pub fn recv(&self) -> Result<T, RecvError> {
+        self.0.recv()
+    }
+}
