6db8dc18f8d3186b03febcb704e47e6e05f93949
Bruno Dutra <brunocodutra@gmail.com>
2019-04-11 22:32:54 +0200

    refactor Buffer as RingBuffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -3,11 +3,11 @@ use derivative::Derivative;
 
 #[derive(Derivative)]
 #[derivative(Debug)]
-pub struct Buffer<T>(#[derivative(Debug = "ignore")] ArrayQueue<T>);
+pub struct RingBuffer<T>(#[derivative(Debug = "ignore")] ArrayQueue<T>);
 
-impl<T> Buffer<T> {
+impl<T> RingBuffer<T> {
     pub fn new(capacity: usize) -> Self {
-        Buffer(ArrayQueue::new(capacity))
+        RingBuffer(ArrayQueue::new(capacity))
     }
 
     #[cfg(test)]
@@ -15,8 +15,11 @@ impl<T> Buffer<T> {
         self.0.capacity()
     }
 
-    pub fn push(&self, value: T) -> Option<T> {
-        self.0.push(value).err().map(|PushError(value)| value)
+    pub fn push(&self, mut value: T) {
+        while let Err(PushError(v)) = self.0.push(value) {
+            self.pop();
+            value = v;
+        }
     }
 
     pub fn pop(&self) -> Option<T> {
@@ -28,41 +31,28 @@ impl<T> Buffer<T> {
 mod tests {
     use super::*;
     use proptest::{collection::vec, prelude::*};
-    use std::cmp::{max, min};
+    use std::cmp::max;
 
     proptest! {
         #[test]
         fn capacity(capacity in 1..=100usize) {
-            let buffer = Buffer::<()>::new(capacity);
+            let buffer = RingBuffer::<()>::new(capacity);
             assert_eq!(buffer.capacity(), capacity);
         }
 
         #[test]
-        fn push(entries in vec(any::<u32>(), 1..=100), capacity in 1..=100usize) {
-            let buffer = Buffer::new(capacity);
-
-            for entry in entries.iter().take(min(entries.len(), capacity)) {
-                assert_eq!(buffer.push(entry), None);
-            }
-
-            for entry in entries.iter().take(max(entries.len(), capacity)).skip(capacity) {
-                assert_eq!(buffer.push(entry), Some(entry));
-            }
-        }
-
-        #[test]
-        fn pop(entries in vec(any::<u32>(), 1..=100), capacity in 1..=100usize) {
-            let buffer = Buffer::new(capacity);
+        fn push_pop(entries in vec(any::<u32>(), 1..=100), capacity in 1..=100usize) {
+            let buffer = RingBuffer::new(capacity);
 
             for &entry in &entries {
                 buffer.push(entry);
             }
 
-            for &entry in entries.iter().take(min(entries.len(), capacity)) {
+            for &entry in entries.iter().skip(max(entries.len(), capacity) - capacity) {
                 assert_eq!(buffer.pop(), Some(entry));
             }
 
-            for _ in capacity..max(entries.len(), capacity) {
+            for _ in entries.len()..max(entries.len(), capacity) {
                 assert_eq!(buffer.pop(), None);
             }
         }
