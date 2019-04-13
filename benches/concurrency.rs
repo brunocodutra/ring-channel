d2f3a48306d1ccefd3bb582824280ac1338f843a
Bruno Dutra <brunocodutra@gmail.com>
2019-04-13 18:18:45 +0200

    benchmark acquiring and releasing endpoints

---
+++ benches/concurrency.rs
@@ -0,0 +1,39 @@
+use criterion::*;
+use rayon::{current_num_threads, scope};
+use ring_channel::*;
+
+fn concurrency(c: &mut Criterion) {
+    let cardinality = 10000;
+    let concurrency = current_num_threads();
+
+    c.bench(
+        "concurrency",
+        Benchmark::new(cardinality.to_string(), move |b| {
+            b.iter_batched_ref(
+                || ring_channel::<usize>(1),
+                |(tx, rx)| {
+                    scope(|s| {
+                        for _ in 0..concurrency / 2 {
+                            s.spawn(|_| {
+                                for _ in 0..cardinality / concurrency {
+                                    drop(tx.clone());
+                                }
+                            });
+
+                            s.spawn(|_| {
+                                for _ in 0..cardinality / concurrency {
+                                    drop(rx.clone());
+                                }
+                            });
+                        }
+                    })
+                },
+                BatchSize::SmallInput,
+            );
+        })
+        .throughput(Throughput::Elements(cardinality as u32)),
+    );
+}
+
+criterion_group!(benches, concurrency);
+criterion_main!(benches);
