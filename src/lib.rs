894a30526963a9cfd35d464678eff2e90c963a15
Bruno Dutra <brunocodutra@gmail.com>
2019-09-29 22:43:54 +0200

    replace try_recv() by recv() in examples

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //! tx.send("Hello, world!").unwrap();
 //!
 //! // Receive the message through the outbound endpoint.
-//! assert_eq!(rx.try_recv(), Ok("Hello, world!"));
+//! assert_eq!(rx.recv(), Ok("Hello, world!"));
 //! ```
 //!
 //! # Communicating across threads
@@ -58,20 +58,18 @@
 //! let mut rx2 = rx1.clone();
 //!
 //! // Spawn a thread that echoes any message it receives.
-//! thread::spawn(move || loop {
-//!     if let Ok(msg) = rx2.try_recv() {
+//! thread::spawn(move || {
+//!     if let Ok(msg) = rx2.recv() {
 //!         tx2.send(msg).unwrap();
 //!     }
 //! });
 //!
 //! tx1.send("Hello, world!").unwrap();
 //!
-//! loop {
-//!     if let Ok(msg) = rx1.try_recv() {
-//!         // Depending on which thread goes first,
-//!         // we might have received the direct message or the echo.
-//!         break assert_eq!(msg, "Hello, world!");
-//!     }
+//! if let Ok(msg) = rx1.recv() {
+//!     // Depending on which thread goes first,
+//!     // we might have received the direct message or the echo.
+//!     assert_eq!(msg, "Hello, world!");
 //! }
 //! ```
 //!
@@ -99,12 +97,12 @@
 //! drop((tx1, tx2, tx3));
 //!
 //! // Pending messages can still be received.
-//! assert_eq!(rx.try_recv(), Ok(1));
-//! assert_eq!(rx.try_recv(), Ok(2));
-//! assert_eq!(rx.try_recv(), Ok(3));
+//! assert_eq!(rx.recv(), Ok(1));
+//! assert_eq!(rx.recv(), Ok(2));
+//! assert_eq!(rx.recv(), Ok(3));
 //!
 //! // Finally, the channel reports itself as disconnectd.
-//! assert_eq!(rx.try_recv(), Err(TryRecvError::Disconnected));
+//! assert_eq!(rx.recv(), Err(RecvError::Disconnected));
 //! ```
 //!
 //! # Experimental Features
