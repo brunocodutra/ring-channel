01ab47177d7104440f1fa2f11498d4a0fe3cbac6
Bruno Dutra <brunocodutra@gmail.com>
2020-03-10 21:43:20 +0100

    improve fuzzer code coverage

--- fuzz/fuzz_targets/fuzzer.rs
+++ fuzz/fuzz_targets/fuzzer.rs
@@ -1,36 +1,39 @@
 #![no_main]
-use futures::{executor::*, future::*, prelude::*, stream::iter, stream::*, task::*};
+use futures::{executor::*, future::*, prelude::*, stream::*, task::*};
 use lazy_static::lazy_static;
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{arbitrary, fuzz_target};
 use ring_channel::*;
-use std::{collections::HashSet, iter::from_fn, num::NonZeroUsize};
+use std::{collections::HashSet, iter::repeat, num::NonZeroUsize};
+
+#[derive(Debug, arbitrary::Arbitrary)]
+struct Input {
+    data: Vec<u32>,
+    capacity: u16,
+    senders: u8,
+    receivers: u8,
+}
 
 lazy_static! {
     static ref POOL: ThreadPool = ThreadPool::new().unwrap();
 }
 
-fuzz_target!(|input: (Vec<u8>, u8, u8, u16)| {
-    let (mut data, senders, receivers, capacity) = input;
-    let (senders, receivers, capacity) = (senders as usize, receivers as usize, capacity as usize);
+fuzz_target!(|input: Input| {
+    let mut data = input.data;
+    let capacity = input.capacity as usize;
+    let senders = input.senders as usize;
+    let receivers = input.receivers as usize;
 
     if let Some(capacity) = NonZeroUsize::new(capacity) {
-        let (tx, rx) = ring_channel::<u8>(capacity);
+        let (tx, rx) = ring_channel(capacity);
         let (mut txs, mut rxs) = (vec![tx; senders], vec![rx; receivers]);
 
-        let chunks: Box<dyn Iterator<Item = &[u8]>> = if !data.is_empty() && senders > 0 {
-            Box::new(data.chunks((data.len() + senders - 1) / senders))
+        let chunk_size = if !data.is_empty() && senders > 0 {
+            (data.len() + senders - 1) / senders
         } else {
-            Box::new(from_fn(|| Some(&[] as &[u8])))
+            1
         };
 
-        let (results, mut received) = block_on(join(
-            join_all(
-                txs.drain(..)
-                    .zip(chunks)
-                    .map(|(tx, data)| iter(data.to_owned()).map(Ok).forward(tx))
-                    .map(|fut| POOL.spawn_with_handle(fut))
-                    .map(Result::unwrap),
-            ),
+        let (mut received, results) = block_on(join(
             join_all(
                 rxs.drain(..)
                     .map(StreamExt::collect::<Vec<_>>)
@@ -40,21 +43,44 @@ fuzz_target!(|input: (Vec<u8>, u8, u8, u16)| {
             .map(IntoIterator::into_iter)
             .map(Iterator::flatten)
             .map(Iterator::collect::<Vec<_>>),
+            join_all(
+                txs.drain(..)
+                    .zip(data.chunks(chunk_size).chain(repeat([].as_ref())))
+                    .map(|(tx, data)| iter(data.to_owned()).map(Ok).forward(tx))
+                    .map(|fut| POOL.spawn_with_handle(fut))
+                    .map(Result::unwrap),
+            ),
         ));
 
-        if senders > 0 && receivers > 0 {
-            for result in results {
-                assert_eq!(result, Ok(()));
-            }
+        if receivers == 0 {
+            assert_eq!(
+                results,
+                data.chunks(chunk_size)
+                    .map(<[_]>::first)
+                    .map(Option::unwrap)
+                    .cloned()
+                    .map(SendError::Disconnected)
+                    .map(Err)
+                    .chain(repeat(Ok(())))
+                    .take(results.len())
+                    .collect::<Vec<_>>(),
+                "Sending fails with disconnection error"
+            );
+        } else if senders > 0 {
+            assert_eq!(results, vec![Ok(()); results.len()], "Sending succeeds");
 
             if capacity.get() >= data.len() {
                 data.sort();
                 received.sort();
-                assert_eq!(received, data);
+                assert_eq!(received, data, "Data received equals data sent");
             } else {
-                let data = data.drain(..).collect::<HashSet<_>>();
-                let received = received.drain(..).collect::<HashSet<_>>();
-                assert_eq!(received, received.intersection(&data).cloned().collect());
+                let data: HashSet<_> = data.drain(..).collect();
+                let received: HashSet<_> = received.drain(..).collect();
+                assert_eq!(
+                    received,
+                    received.intersection(&data).cloned().collect(),
+                    "Data received is a subset of data sent"
+                );
             }
         }
     }
