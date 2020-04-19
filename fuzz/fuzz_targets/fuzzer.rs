93ac161760ea4fcdbf5759432b261193f2f359d0
Bruno Dutra <brunocodutra@gmail.com>
2020-04-19 16:48:29 +0200

    update dependencies

--- fuzz/fuzz_targets/fuzzer.rs
+++ fuzz/fuzz_targets/fuzzer.rs
@@ -1,31 +1,24 @@
 #![no_main]
+
 use futures::{executor::*, future::*, prelude::*, stream::*, task::*};
 use lazy_static::lazy_static;
-use libfuzzer_sys::{arbitrary, fuzz_target};
+use libfuzzer_sys::fuzz_target;
 use ring_channel::*;
 use std::{collections::HashSet, iter::repeat, num::NonZeroUsize};
-
-#[derive(Debug, arbitrary::Arbitrary)]
-struct Input {
-    data: Vec<u32>,
-    capacity: u16,
-    senders: u8,
-    receivers: u8,
-}
 
 lazy_static! {
     static ref POOL: ThreadPool = ThreadPool::new().unwrap();
 }
 
-fuzz_target!(|input: Input| {
-    let mut data = input.data;
-    let capacity = input.capacity as usize;
-    let senders = input.senders as usize;
-    let receivers = input.receivers as usize;
+fuzz_target!(|input: (Box<[u32]>, u16, u8, u8)| {
+    let mut data = input.0;
+    let capacity = input.1 as usize;
+    let senders = input.2 as usize;
+    let receivers = input.3 as usize;
 
     if let Some(capacity) = NonZeroUsize::new(capacity) {
         let (tx, rx) = ring_channel(capacity);
-        let (mut txs, mut rxs) = (vec![tx; senders], vec![rx; receivers]);
+        let (txs, rxs) = (vec![tx; senders], vec![rx; receivers]);
 
         let chunk_size = if !data.is_empty() && senders > 0 {
             (data.len() + senders - 1) / senders
@@ -35,16 +28,16 @@ fuzz_target!(|input: Input| {
 
         let (mut received, results) = block_on(join(
             join_all(
-                rxs.drain(..)
+                rxs.into_iter()
                     .map(StreamExt::collect::<Vec<_>>)
                     .map(|fut| POOL.spawn_with_handle(fut))
                     .map(Result::unwrap),
             )
             .map(IntoIterator::into_iter)
             .map(Iterator::flatten)
-            .map(Iterator::collect::<Vec<_>>),
+            .map(Iterator::collect::<Box<[_]>>),
             join_all(
-                txs.drain(..)
+                txs.into_iter()
                     .zip(data.chunks(chunk_size).chain(repeat([].as_ref())))
                     .map(|(tx, data)| iter(data.to_owned()).map(Ok).forward(tx))
                     .map(|fut| POOL.spawn_with_handle(fut))
@@ -74,8 +67,8 @@ fuzz_target!(|input: Input| {
                 received.sort();
                 assert_eq!(received, data, "Data received equals data sent");
             } else {
-                let data: HashSet<_> = data.drain(..).collect();
-                let received: HashSet<_> = received.drain(..).collect();
+                let data: HashSet<_> = data.into_iter().collect();
+                let received: HashSet<_> = received.into_iter().collect();
                 assert_eq!(
                     received,
                     received.intersection(&data).cloned().collect(),
