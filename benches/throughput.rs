4ef343708a210ba5925c3cb92f700617a4dc4fc6
Bruno Dutra <brunocodutra@gmail.com>
2022-02-12 22:01:18 +0100

    replace async runtime from smol to tokio

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -1,92 +1,82 @@
-use async_trait::async_trait;
 use criterion::measurement::{Measurement, WallTime};
-use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use futures::{future::join, prelude::*, stream::repeat};
+use criterion::{criterion_group, criterion_main, BenchmarkGroup, Criterion, Throughput};
+use futures::future::{try_join, try_join_all};
+use futures::prelude::*;
 use ring_channel::{ring_channel, RingReceiver, RingSender, TryRecvError};
-use smol::{block_on, spawn, unblock};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::{env::set_var, hint::spin_loop, iter, mem::size_of, num::NonZeroUsize};
+use std::{fmt::Debug, hint::spin_loop, iter, mem::size_of, num::NonZeroUsize, time::Duration};
+use tokio::runtime::Runtime;
+use tokio::task::{self, JoinHandle};
 
 #[cfg(feature = "futures_api")]
 use futures::{stream::unfold, SinkExt};
 
-const MESSAGES: usize = 100;
-
-#[async_trait]
 trait Routine<T> {
-    async fn produce(tx: RingSender<T>, limit: usize) -> usize;
-    async fn consume(rx: RingReceiver<T>, limit: usize) -> usize;
+    fn produce(tx: RingSender<T>, limit: usize) -> JoinHandle<usize>;
+    fn consume(rx: RingReceiver<T>, limit: usize) -> JoinHandle<usize>;
 }
 
-fn bench<R: Routine<[char; N]>, const N: usize>(c: &mut Criterion, name: &str, m: usize, n: usize) {
-    set_var("SMOL_THREADS", num_cpus::get().to_string());
+async fn run<R, const N: usize>(msgs: usize, cap: usize, p: usize, c: usize) -> Duration
+where
+    R: Routine<[char; N]>,
+{
+    let (tx, rx) = ring_channel(NonZeroUsize::new(cap).unwrap());
+    let producer = try_join_all(iter::repeat(tx).take(p).map(|tx| R::produce(tx, msgs / p)));
+    let consumer = try_join_all(iter::repeat(rx).take(c).map(|rx| R::consume(rx, msgs / c)));
 
-    let mut group = c.benchmark_group(name);
-    group.throughput(Throughput::Elements(MESSAGES as u64));
+    let checkpoint = WallTime.start();
+    let (produced, consumed) = try_join(producer, consumer).await.unwrap();
+    let elapsed = WallTime.end(checkpoint);
 
-    for &cap in &[1, m + n] {
-        group.bench_function(
-            format!("{}B/{}x{}/{}", size_of::<[char; N]>(), m, n, cap),
-            |b| {
-                b.iter_custom(|iters| {
-                    let produced = AtomicUsize::new(0);
-                    let consumed = AtomicUsize::new(0);
-                    let checkpoint = WallTime.start();
+    elapsed.div_f64(consumed.iter().sum::<usize>() as f64 / produced.iter().sum::<usize>() as f64)
+}
 
-                    let (tx, rx) = ring_channel(NonZeroUsize::new(cap).unwrap());
+fn bench<R, const N: usize>(group: &mut BenchmarkGroup<WallTime>, p: usize, c: usize)
+where
+    R: Debug + Default + Routine<[char; N]>,
+{
+    let prefix = format!("{:?}/{}B/{}x{}", R::default(), size_of::<[char; N]>(), p, c);
 
-                    block_on(join(
-                        repeat(tx).take(m).for_each_concurrent(None, |tx| async {
-                            produced.fetch_add(
-                                spawn(R::produce(tx, iters as usize * MESSAGES / m)).await,
-                                Ordering::Relaxed,
-                            );
-                        }),
-                        repeat(rx).take(n).for_each_concurrent(None, |rx| async {
-                            consumed.fetch_add(
-                                spawn(R::consume(rx, iters as usize * MESSAGES / n)).await,
-                                Ordering::Relaxed,
-                            );
-                        }),
-                    ));
+    let messages_per_iter = 256 * p;
+    group.throughput(Throughput::Elements(messages_per_iter as u64));
 
-                    let elapsed = WallTime.end(checkpoint);
-                    elapsed.div_f64(consumed.into_inner() as f64 / produced.into_inner() as f64)
-                });
-            },
-        );
+    for &cap in &[1, p + c] {
+        group.bench_function(format!("{}/{}", prefix, cap), |b| {
+            b.to_async(Runtime::new().unwrap())
+                .iter_custom(|iters| run::<R, N>(iters as usize * messages_per_iter, cap, p, c));
+        });
     }
 }
 
 #[cfg(feature = "futures_api")]
+#[derive(Debug, Default)]
 struct Async;
 
 #[cfg(feature = "futures_api")]
-#[async_trait]
 impl<T: 'static + Send + Default> Routine<T> for Async {
-    async fn produce(tx: RingSender<T>, limit: usize) -> usize {
+    fn produce(tx: RingSender<T>, limit: usize) -> JoinHandle<usize> {
         let producer = unfold(tx, |mut tx| async move {
             <_ as SinkExt<_>>::send(&mut tx, T::default()).await.ok()?;
             Some(((), tx))
         });
 
-        producer.take(limit).count().await
+        task::spawn(producer.take(limit).count())
     }
 
-    async fn consume(rx: RingReceiver<T>, limit: usize) -> usize {
-        rx.take(limit).count().await
+    fn consume(rx: RingReceiver<T>, limit: usize) -> JoinHandle<usize> {
+        task::spawn(rx.take(limit).count())
     }
 }
+
+#[derive(Debug, Default)]
 struct Block;
 
-#[async_trait]
 impl<T: 'static + Send + Default> Routine<T> for Block {
-    async fn produce(mut tx: RingSender<T>, limit: usize) -> usize {
+    fn produce(mut tx: RingSender<T>, limit: usize) -> JoinHandle<usize> {
         let producer = iter::from_fn(move || tx.send(T::default()).ok());
-        unblock(move || producer.take(limit).count()).await
+        task::spawn_blocking(move || producer.take(limit).count())
     }
 
-    async fn consume(mut rx: RingReceiver<T>, limit: usize) -> usize {
+    fn consume(mut rx: RingReceiver<T>, limit: usize) -> JoinHandle<usize> {
         let consumer = iter::from_fn(move || loop {
             match rx.try_recv() {
                 Ok(m) => return Some(m),
@@ -95,54 +85,71 @@ impl<T: 'static + Send + Default> Routine<T> for Block {
             }
         });
 
-        unblock(move || consumer.take(limit).count()).await
+        task::spawn_blocking(move || consumer.take(limit).count())
     }
 }
 
 fn mpmc(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mpmc");
     let concurrency = (num_cpus::get() / 2).max(2);
 
     #[cfg(feature = "futures_api")]
-    bench::<Async, 1>(c, "async/mpmc", concurrency, concurrency);
-    bench::<Block, 1>(c, "block/mpmc", concurrency, concurrency);
+    {
+        bench::<Async, 1>(&mut group, concurrency, concurrency);
+        bench::<Async, 8>(&mut group, concurrency, concurrency);
+    }
 
-    #[cfg(feature = "futures_api")]
-    bench::<Async, 8>(c, "async/mpmc", concurrency, concurrency);
-    bench::<Block, 8>(c, "block/mpmc", concurrency, concurrency);
+    {
+        bench::<Block, 1>(&mut group, concurrency, concurrency);
+        bench::<Block, 8>(&mut group, concurrency, concurrency);
+    }
 }
 
 fn mpsc(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mpsc");
     let concurrency = (num_cpus::get() - 1).max(2);
 
     #[cfg(feature = "futures_api")]
-    bench::<Async, 1>(c, "async/mpsc", concurrency, 1);
-    bench::<Block, 1>(c, "block/mpsc", concurrency, 1);
+    {
+        bench::<Async, 1>(&mut group, concurrency, 1);
+        bench::<Async, 8>(&mut group, concurrency, 1);
+    }
 
-    #[cfg(feature = "futures_api")]
-    bench::<Async, 8>(c, "async/mpsc", concurrency, 1);
-    bench::<Block, 8>(c, "block/mpsc", concurrency, 1);
+    {
+        bench::<Block, 1>(&mut group, concurrency, 1);
+        bench::<Block, 8>(&mut group, concurrency, 1);
+    }
 }
 
 fn spmc(c: &mut Criterion) {
+    let mut group = c.benchmark_group("spmc");
     let concurrency = (num_cpus::get() - 1).max(2);
 
     #[cfg(feature = "futures_api")]
-    bench::<Async, 1>(c, "async/spmc", 1, concurrency);
-    bench::<Block, 1>(c, "block/spmc", 1, concurrency);
+    {
+        bench::<Async, 1>(&mut group, 1, concurrency);
+        bench::<Async, 8>(&mut group, 1, concurrency);
+    }
 
-    #[cfg(feature = "futures_api")]
-    bench::<Async, 8>(c, "async/spmc", 1, concurrency);
-    bench::<Block, 8>(c, "block/spmc", 1, concurrency);
+    {
+        bench::<Block, 1>(&mut group, 1, concurrency);
+        bench::<Block, 8>(&mut group, 1, concurrency);
+    }
 }
 
 fn spsc(c: &mut Criterion) {
-    #[cfg(feature = "futures_api")]
-    bench::<Async, 1>(c, "async/spsc", 1, 1);
-    bench::<Block, 1>(c, "block/spsc", 1, 1);
+    let mut group = c.benchmark_group("spsc");
 
     #[cfg(feature = "futures_api")]
-    bench::<Async, 8>(c, "async/spsc", 1, 1);
-    bench::<Block, 8>(c, "block/spsc", 1, 1);
+    {
+        bench::<Async, 1>(&mut group, 1, 1);
+        bench::<Async, 8>(&mut group, 1, 1);
+    }
+
+    {
+        bench::<Block, 1>(&mut group, 1, 1);
+        bench::<Block, 8>(&mut group, 1, 1);
+    }
 }
 
 criterion_group!(benches, mpmc, mpsc, spmc, spsc);
--- src/buffer.rs
+++ src/buffer.rs
@@ -58,105 +58,90 @@ impl<T> RingBuffer<T> {
 mod tests {
     use super::*;
     use crate::{RingReceiver, RingSender};
-    use alloc::{rc::Rc, string::String, sync::Arc, vec::Vec};
-    use core::{cmp::max, mem::discriminant, num::NonZeroUsize, ptr::NonNull};
+    use alloc::{sync::Arc, vec::Vec};
+    use core::{cmp::max, mem::discriminant};
+    use futures::{future::try_join, prelude::*, stream::repeat};
     use proptest::collection::size_range;
     use test_strategy::proptest;
+    use tokio::{runtime, task::spawn_blocking};
 
-    #[test]
-    fn queue() {
+    #[proptest]
+    fn new_uses_atomic_cell_when_possible() {
         assert_eq!(
-            discriminant(&RingBuffer::<String>::new(1)),
+            discriminant(&RingBuffer::<[char; 1]>::new(1)),
+            discriminant(&RingBuffer::Cell(Default::default()))
+        );
+
+        assert_eq!(
+            discriminant(&RingBuffer::<[char; 1]>::new(2)),
+            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
+        );
+
+        assert_eq!(
+            discriminant(&RingBuffer::<[char; 4]>::new(1)),
             discriminant(&RingBuffer::Queue(ArrayQueue::new(1)))
         );
 
         assert_eq!(
-            discriminant(&RingBuffer::<NonZeroUsize>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<NonNull<String>>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<Rc<String>>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<Arc<String>>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<RingReceiver<String>>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<RingSender<String>>::new(2)),
-            discriminant(&RingBuffer::Queue(ArrayQueue::new(2)))
-        );
-    }
-
-    #[test]
-    fn cell() {
-        assert_eq!(
-            discriminant(&RingBuffer::<NonZeroUsize>::new(1)),
+            discriminant(&RingBuffer::<RingSender<()>>::new(1)),
             discriminant(&RingBuffer::Cell(Default::default()))
         );
 
         assert_eq!(
-            discriminant(&RingBuffer::<NonNull<String>>::new(1)),
-            discriminant(&RingBuffer::Cell(Default::default()))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<Rc<String>>::new(1)),
-            discriminant(&RingBuffer::Cell(Default::default()))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<Arc<String>>::new(1)),
-            discriminant(&RingBuffer::Cell(Default::default()))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<RingReceiver<String>>::new(1)),
-            discriminant(&RingBuffer::Cell(Default::default()))
-        );
-
-        assert_eq!(
-            discriminant(&RingBuffer::<RingSender<String>>::new(1)),
+            discriminant(&RingBuffer::<RingReceiver<()>>::new(1)),
             discriminant(&RingBuffer::Cell(Default::default()))
         );
     }
 
     #[proptest]
-    fn capacity(#[strategy(1..=100usize)] capacity: usize) {
+    fn capacity_returns_the_maximum_buffer_size(#[strategy(1..=100usize)] capacity: usize) {
         let buffer = RingBuffer::<()>::new(capacity);
         assert_eq!(buffer.capacity(), capacity);
     }
 
     #[proptest]
-    fn overflow(
-        #[any(size_range(1..=100).lift())] entries: Vec<char>,
+    fn oldest_items_are_overwritten_on_overflow(
+        #[any(size_range(1..=100).lift())] items: Vec<char>,
         #[strategy(1..=100usize)] capacity: usize,
     ) {
         let buffer = RingBuffer::new(capacity);
 
-        for &entry in &entries {
-            buffer.push(entry);
+        for &item in &items {
+            buffer.push(item);
         }
 
-        for &entry in entries.iter().skip(max(entries.len(), capacity) - capacity) {
-            assert_eq!(buffer.pop(), Some(entry));
+        for &item in items.iter().skip(max(items.len(), capacity) - capacity) {
+            assert_eq!(buffer.pop(), Some(item));
         }
 
-        for _ in entries.len()..max(entries.len(), capacity) {
+        for _ in items.len()..max(items.len(), capacity) {
             assert_eq!(buffer.pop(), None);
         }
+    }
+
+    #[proptest]
+    fn buffer_is_thread_safe(
+        #[strategy(1..=100usize)] m: usize,
+        #[strategy(1..=100usize)] n: usize,
+        #[strategy(1..=100usize)] capacity: usize,
+    ) {
+        let rt = runtime::Builder::new_multi_thread().build()?;
+        let buffer = Arc::new(RingBuffer::new(capacity));
+
+        rt.block_on(try_join(
+            repeat(buffer.clone())
+                .enumerate()
+                .take(m)
+                .map(Ok)
+                .try_for_each_concurrent(None, |(item, b)| spawn_blocking(move || b.push(item))),
+            repeat(buffer)
+                .take(n)
+                .map(Ok)
+                .try_for_each_concurrent(None, |b| {
+                    spawn_blocking(move || {
+                        b.pop();
+                    })
+                }),
+        ))?;
     }
 }
